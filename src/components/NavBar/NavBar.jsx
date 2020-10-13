8483aa9e984e8e7aeabec46099cedc8402c5c4c2
Kathleen Stickel <kstick9210@outlook.com>
2020-10-12 19:42:44 -0500

    update nav styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,38 +3,29 @@ import {Link} from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = ({ user, handleLogout }) => {
-  let nav = user ? 
-    <>
-      <nav className="nav-bar">
-        <Link to="/"><img 
+  return (
+    <nav className="nav-bar">
+    <div className="logo-name-wrapper">
+        <Link to="/" className="logo-name-link">
+          <img 
           src="https://i.imgur.com/J0GyZIF.png" 
           alt="Locally Caffeinated Logo"
           className="logo"/>
+          <h1 className="app-name">Locally Caffeinated</h1>
         </Link>
+      </div>
+      {user ? 
         <div className="nav-wrapper">
           <Link to={`/user/${user._id}`} className="nav-link">Welcome, {user.name}</Link>
           <Link to=" " onClick={handleLogout} className="nav-link">Log Out</Link>
         </div>
-      </nav>
-    </>
-    :
-    <>
-      <nav className="nav-bar">
-        <Link to="/"><img 
-          src="https://i.imgur.com/J0GyZIF.png" 
-          alt="Locally Caffeinated Logo"
-          className="logo"/>
-        </Link>
-        <h1 className="app-name">Locally Caffeinated</h1>
+        :
         <div className="nav-wrapper">
           <Link to="/login" className="nav-link">Log In</Link>
           <Link to="/signup" className="nav-link">Sign Up</Link>
         </div>
-      </nav>
-    </>
-
-  return (
-    nav
+      }
+    </nav>
   );
 };
 
