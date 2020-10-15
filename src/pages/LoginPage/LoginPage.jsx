4273fc451d40b54966037b6b97575216ff2f5d5f
Kathleen Stickel <kstick9210@outlook.com>
2020-10-15 18:34:28 -0500

    remove materialize classes

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -27,27 +27,19 @@ const LoginPage = ({history, handleSignupOrLogin}) => {
   
   return (
     <div className="LoginPage">
-      <h3>Log In</h3>
-      <form className="col s12" autoComplete="off" onSubmit={handleSubmit} >
-        <div className="row">
-          <div className="input-field col s12">
-            <input type="text" autoComplete="off" id="email" className="active" value={loginInfo.email} name="email" onChange={handleChange} />
+      <h1>Log In</h1>
+      <form className="login-form" autoComplete="off" onSubmit={handleSubmit} >
+          <div>
+            <input type="text" autoComplete="off" id="email" className="active login-input" value={loginInfo.email} name="email" onChange={handleChange} />
             <label htmlFor="email">Email</label>
           </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input type="password" autoComplete="off" className="active" id="password" value={loginInfo.password} name="password" onChange={handleChange} />
+          <div>
+            <input type="password" autoComplete="off" className="active login-input" id="password" value={loginInfo.password} name="password" onChange={handleChange} />
             <label htmlFor="password">Password</label>
           </div>
-        </div>
-        <div className="row">
-          <div className="col s12">
-            <button className="btn green">Log In<i className="material-icons right">arrow_forward</i></button>&nbsp;&nbsp;&nbsp;
-            <Link className="btn red" to='/'>Cancel<i className="material-icons right">cancel</i></Link>
-          </div>
-        </div>
+          <button id="login-button">Log In</button>
       </form>
+      <Link to='/'>Cancel</Link>
     </div>
   );
 }
