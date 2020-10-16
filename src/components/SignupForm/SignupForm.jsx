31de5d749d3a7882e5abf43d9f23345ac8a39abb
Kathleen Stickel <kstick9210@outlook.com>
2020-10-16 10:32:29 -0500

    restyle signup form

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,7 +1,7 @@
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import userService from '../../services/userService';
+import './SignupForm.css';
 
 const SignupForm = ({history, handleSignupOrLogin, updateMessage}) => {
 
@@ -34,33 +34,15 @@ const SignupForm = ({history, handleSignupOrLogin, updateMessage}) => {
   
   return (
     <div>
-      <h3>Sign Up</h3>
-      <form className="col s12" autoComplete="off" onSubmit={handleSubmit} >
-        <div className="row">
-          <div className="input-field col s12">
-            <input type="text" autoComplete="off" className="active" id="name" value={signupInfo.name} name="name" onChange={handleChange} placeholder="Name"/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input type="text" autoComplete="off" className="active" id="email" value={signupInfo.email} name="email" onChange={handleChange} placeholder="Email"/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input type="password" autoComplete="off" className="active" id="password" value={signupInfo.password} name="password" onChange={handleChange} placeholder="Password"/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s12">
-            <input type="password" autoComplete="off" className="active" id="confirm" value={signupInfo.passwordConf} name="passwordConf" onChange={handleChange} placeholder="Confirm Password"/>
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="col-sm-12 text-center">
-            <button className="btn green" disabled={isFormInvalid()}>Sign Up</button>&nbsp;&nbsp;
-            <Link className="btn red" to='/'>Cancel</Link>
-          </div>
+      <h1 className="signup-title">Sign Up</h1>
+      <form className="signup-form" autoComplete="off" onSubmit={handleSubmit} >
+        <input type="text" autoComplete="off" className="active signup-input" id="name" value={signupInfo.name} name="name" onChange={handleChange} placeholder="Name"/>
+        <input type="text" autoComplete="off" className="active signup-input" id="email" value={signupInfo.email} name="email" onChange={handleChange} placeholder="Email"/>
+        <input type="password" autoComplete="off" className="active signup-input" id="password" value={signupInfo.password} name="password" onChange={handleChange} placeholder="Password"/>
+        <input type="password" autoComplete="off" className="active signup-input" id="confirm" value={signupInfo.passwordConf} name="passwordConf" onChange={handleChange} placeholder="Confirm Password"/>
+        <div className="btn-wrapper">
+          <button disabled={isFormInvalid()}>Sign Up</button>
+          <Link to="/">Cancel</Link>
         </div>
       </form>
     </div>
