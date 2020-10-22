1349b4c8cff5f3856b1adb201f2dc63100db137e
Kathleen Stickel <kstick9210@outlook.com>
2020-10-21 19:13:22 -0500

    add backend functionality to create shop

--- controllers/shops.js
+++ controllers/shops.js
@@ -1,5 +1,12 @@
 const Shop = require('../models/shop');
 
 module.exports = {
-    
+    create
+}
+
+function create(req, res) {
+    req.body.user = req.user._id;
+    Shop.create(req.body)
+    .then(shop => {res.json(shop)})
+    .catch(err => {res.json(err)})
 }
--- routes/shops.js
+++ routes/shops.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 const shopsCtrl = require('../controllers/shops');
 
+router.post('/', shopsCtrl.create);
+
 module.exports = router;
