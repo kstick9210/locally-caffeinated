6b72bcc3ebf726c9cdff05e90942a8d37274b94d
Kathleen Stickel <kstick9210@outlook.com>
2020-11-04 16:48:54 -0600

    add axios and update google api services

--- src/services/googleplaces-api.js
+++ src/services/googleplaces-api.js
@@ -1,10 +1,18 @@
-const BASE_URL = '/api/googleplaces';
+const axios = require('axios');
+const BASE_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json?';
+const key = process.env.GOOGLE_API_KEY;
 
-export function search(formData) {
-    return fetch(BASE_URL, {
-        method: 'POST',
-        headers: {'content-type': 'application/json'},
-        body: JSON.stringify(formData)
-    }, {mode: "cors"})
-    .then(res => res.json())
+// export function search(searchTerm) {
+//     return fetch(BASE_URL, {
+//         method: 'POST',
+//         headers: {'content-type': 'application/json'},
+//         body: JSON.stringify(searchTerm)
+//     }, {mode: "cors"})
+//     .then(res => res.json())
+// }
+
+export function search(req, res) {
+    axios.get(`${BASE_URL}query=coffee+shop+${req.body.query}&key=${key}`)
+    .then(response => {res.json(response.data)})
+    .catch((err) => console.log(err));
 }
