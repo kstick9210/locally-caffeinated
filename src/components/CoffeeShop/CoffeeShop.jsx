1b5929de86586cbbf43364b698750349246307ff
Kathleen Stickel <kstick9210@outlook.com>
2020-11-08 12:19:39 -0600

    update search results styling

--- src/components/CoffeeShop/CoffeeShop.jsx
+++ src/components/CoffeeShop/CoffeeShop.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import "./CoffeeShop.css";
 
 export default function CoffeeShop({shop}) {
     return (
-        <div>
-            <h1>{shop.name}</h1>
+        <div className="CoffeeShop">
+            <h1 className="shop-name">
+                <img src={shop.icon} alt="google places icon" className="places-icon"/>
+                &nbsp;{shop.name}
+            </h1>
+            <h3>{shop.rating}</h3>
         </div>
     )
 }
--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -5,6 +5,14 @@ import CoffeeShop from '../CoffeeShop/CoffeeShop';
 const SearchResults = ({ shops }) => {
     return (
         <div className="SearchResults">
+            <div>
+                <h2>
+                    <img src="https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/cafe-71.png" className="places-icon" alt="cafe icon"/>
+                    &nbsp;Cafe<br />
+                    <img src="https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png" className="places-icon" alt="restaurant icon" />
+                    &nbsp;Restaurant
+                    </h2>
+            </div>
             {shops.map((shop, idx) =>
                 <CoffeeShop 
                     key={idx} 
