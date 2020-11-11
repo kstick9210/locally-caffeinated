6330044899c14d49c1a3540f949065427fcd19a9
Kathleen Stickel <kstick9210@outlook.com>
2020-11-11 10:51:36 -0600

    edit landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import {Link} from "react-router-dom";
 
 import "./LandingPage.css";
 
@@ -7,6 +8,14 @@ export default function LandingPage() {
         <div className="LandingPage-wrapper">
             <div className="LandingPage">
                 <h1>Locally Caffeinated</h1>
+                <h3>
+                    <Link to="/search" className="underline">Search for coffe shops</Link> 
+                    &nbsp;in any city around the world.
+                </h3>
+                <h3>
+                    <Link to="/signup" className="underline">Create an account</Link>
+                    &nbsp;to save your favorites.
+                </h3>
             </div>
         </div>
     )
