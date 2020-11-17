afd8d77db9a70c0a4847511d879aa3d2f2990af8
Kathleen Stickel <kstick9210@outlook.com>
2020-11-17 10:21:11 -0600

    update server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/auth', authRouter);
 app.use('/api/shops', shopsRouter);
